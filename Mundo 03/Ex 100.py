20eea23c09ab0b1fb1d36b6577c2aafa916061b0
Marissa Borges <marissaborges2006@gmail.com>
2025-04-01 08:27:21 -0300

    Realizando melhorias no 100° exercício

--- Mundo 03/Ex 100.py
+++ Mundo 03/Ex 100.py
@@ -4,24 +4,23 @@
 from random import randint
 from time import sleep
 
-numeros = []
-
-def sortear():
+def sortear(lista):
     print('Sorteando 5 valores da lista ', end='')
     for c in range(0, 5):
-        num = randint(1, 20)
+        num = randint(1, 10)
         print(num, end= ' ')
-        numeros.append(num)
+        lista.append(num)
         sleep(0.7)
     print('PRONTO!!')
 
-def somarPar():
-    print(f'Somando os valores pares de {numeros},', end= ' ')
+def somarPar(lista):
+    print(f'Somando os valores pares de {lista},', end= ' ')
     s = 0
-    for n in numeros:
+    for n in lista:
         if n % 2 == 0:
             s += n
     print(f'temos {s}')
 
-sortear()
-somarPar()
+numeros = []
+sortear(numeros)
+somarPar(numeros)
