6bcd08a0332663044cad70df3522800a120bbbea
Marissa Borges <marissaborges2006@gmail.com>
2025-04-08 13:14:33 -0300

    Salvando os exercícios  101 e 102 da aula 21 parte 2

--- Mundo 03/Ex 101.py
+++ Mundo 03/Ex 101.py
@@ -1,4 +1,20 @@
 # -----------------------------------------------
 #    EXERCÍCIO 101
 # -----------------------------------------------
+from datetime import date
 
+def voto(ano):
+    hoje = date.today().year
+    idade = hoje - ano
+    if idade >= 18 and idade < 65:
+        eleicao = 'VOTO OBRIGATÓRIO'
+    elif idade < 16:
+        eleicao = 'NÃO VOTA'
+    else:
+        eleicao = 'VOTO OPCIONAL'
+    return f'Com {idade} anos: {eleicao}'
+
+print('-'*30)
+ano = int(input('Em que ano você nasceu? '))
+
+print(voto(ano))
--- Mundo 03/Ex 102.py
+++ Mundo 03/Ex 102.py
@@ -2,3 +2,20 @@
 #    EXERCÍCIO 102
 # -----------------------------------------------
 
+def fatorial(n, show=False):
+    if show == False:
+        f = 1
+        for c in range(n, 0, -1):
+            f *= c
+        return f
+    else:
+        f = 1
+        for c in range(n, 0, -1):
+            f *= c
+            print(f'{c} = ' if c == 1 else f'{c} x ', end='')
+        return f
+
+
+
+
+print(fatorial(2, True))
