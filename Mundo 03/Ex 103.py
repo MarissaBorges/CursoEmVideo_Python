1226a7914dee82107241719912c0190cc43212a1
Marissa Borges <marissaborges2006@gmail.com>
2025-04-09 15:26:02 -0300

    Realizando a melhoria do exercício 103

--- Mundo 03/Ex 103.py
+++ Mundo 03/Ex 103.py
@@ -2,19 +2,16 @@
 #    EXERCÍCIO 103
 # -----------------------------------------------
 
-def ficha(nome=0, gols=0):
-    nome = '<desconhecido>'
-    gols = 0
+def ficha(nome='<desconhecido>', gols=0):
     jogador = str(input('Nome do Jogador: '))
     gol = input('Número de Gols: ')
     if jogador != '':
         nome = jogador
-    if gol != '':
+    if gol.isnumeric():
         gols = gol
     
     return f'O jogador {nome} fez {gols} gol(s) no campeonato.'
     
-
 
 print('-'*30)
 print(ficha())
