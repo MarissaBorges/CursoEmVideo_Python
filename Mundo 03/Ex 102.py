b9dcd338c5e57aad00425bfad520830385d4b940
Marissa Borges <marissaborges2006@gmail.com>
2025-04-09 15:15:35 -0300

    Inserindo a docstring e otimizanddo o exercício 102

--- Mundo 03/Ex 102.py
+++ Mundo 03/Ex 102.py
@@ -3,19 +3,19 @@
 # -----------------------------------------------
 
 def fatorial(n, show=False):
-    if show == False:
-        f = 1
-        for c in range(n, 0, -1):
-            f *= c
-        return f
-    else:
-        f = 1
-        for c in range(n, 0, -1):
-            f *= c
+    '''
+    -> Calcula o fatorial de um número.
+    :param n: O número a ser calculado.
+    :param show: (opcional) Mostrar ou não a conta.
+    :return: O valor do Fatorial de um número n.    
+    '''
+    
+    f = 1
+    for c in range(n, 0, -1):
+        if show:
             print(f'{c} = ' if c == 1 else f'{c} x ', end='')
-        return f
+        f *= c
+    return f
 
-
-
-
-print(fatorial(2, True))
+print(fatorial(5, True))
+help(fatorial)
